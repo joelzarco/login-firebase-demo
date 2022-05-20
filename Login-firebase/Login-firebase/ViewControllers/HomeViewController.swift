e100c3199241df765b127bc98eeaa8572802306a
joelzarco <joelzarco@me.com>
2022-05-20 17:57:36 -0500

    Log out method added in HomeVC

--- Login-firebase/Login-firebase/ViewControllers/HomeViewController.swift
+++ Login-firebase/Login-firebase/ViewControllers/HomeViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
     
     let homeLabel = UILabel()
     var defaults = UserDefaults.standard
+    let logOutButton = UIButton(configuration: .filled())
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +26,48 @@ class HomeViewController: UIViewController {
 extension HomeViewController{
     func style(){
         let userFirstName = defaults.string(forKey: "first")
-        if userFirstName != nil{
-            print(userFirstName)
+        let lastname = defaults.string(forKey: "last")
+        if let lastname = lastname {
+            print(lastname)
         }
+        if let userFirstName = userFirstName {
+            print(userFirstName)
+            homeLabel.text = "Welcome Home \(userFirstName)"
+        }else{
+            homeLabel.text = "Welcome home"
+        }
+        
         view.addSubview(homeLabel)
-        view.backgroundColor = .systemPink
+        view.addSubview(logOutButton)
+        view.backgroundColor = .secondarySystemBackground
         homeLabel.translatesAutoresizingMaskIntoConstraints = false
-        homeLabel.text = "Welcome Home \(userFirstName!)"
+        
         homeLabel.textAlignment = .center
         homeLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        
+        Utilities.styleButton(logOutButton, with: "Log Out")
+        logOutButton.addTarget(self, action: #selector(logOutTapped), for: .primaryActionTriggered)
+
     }
     
     func layout(){
         
         homeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         homeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        logOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logOutButton.topAnchor.constraint(equalTo: homeLabel.topAnchor, constant: 50).isActive = true
         
+    }
+    
+    @objc func logOutTapped(){
+        print("logOut Tapped")
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+//            let mainVC = ViewController()
+            navigationController?.popToRootViewController(animated: true) // thank God!!!
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
 }
